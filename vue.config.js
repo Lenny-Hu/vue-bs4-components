9c97ec29f09deb6ae0de1ca6801128f8ce6ddd6b
hule <hule@yiban.cn>
2019-10-12 17:07:34 +0800

    添加vue配置文件，用于配置打包生成的文档路径

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-10-10 16:37:57
+ * @LastEditTime: 2019-10-10 16:37:57
+ * @LastEditors: your name
+ */
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/'
+}
